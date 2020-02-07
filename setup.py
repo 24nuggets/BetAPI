a7b77b4b06513ce58e5a4d619053aa14339611d9
24nuggets <60763657+24nuggets@users.noreply.github.com>
2020-02-06 20:39:27 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='API',
+   version='1.0',
+   description='A useful module',
+   author='Matt Capriotti',
+   author_email='matthewcapriotti4@gmail.com',
+   packages=['API'],  #same as name
+   
+)
