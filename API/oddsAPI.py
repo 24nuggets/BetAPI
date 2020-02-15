027f99675a44d57982e5c99f3c6dbec0ed1c5261
24nuggets <60763657+24nuggets@users.noreply.github.com>
2020-02-15 10:48:20 -0500

    Update oddsAPI.py

--- API/oddsAPI.py
+++ API/oddsAPI.py
@@ -55,6 +55,61 @@ def nfl():
 	output=json.dumps(dict(masterdata))
 	return output
 
+@app.route('/api/v1/resources/nba/match/spreads/american', methods=['GET'])
+def nba():
+	league='nba'
+	manager=Manager()
+	masterdata=manager.dict()
+	IntertopsBaseURLs='https://sports.intertops.eu/en/Bets/Basketball/NBA-Lines/1070'
+	bovadaAPIUrls='https://services.bovada.lv/services/sports/event/coupon/events/A/description/basketball?marketFilterId=def&preMatchOnly=true&eventsLimit=50&lang=en'
+	betPhoenixAPIUrls='https://api.securecashiersystem.ag/js/Lines.js?jsoncallback=jQuery111207169812656759593_1577303361038&hid=bdb01c9f736f2e641984fcde2fce11ad&cmd=games&leagues=3&nhlLine=A&mblLine=A&lineStyle=E&language=0&_=1577303361039'
+	skyBookAPIUrls='https://api.securecashiersystem.ag/js/Lines.js?jsoncallback=jQuery111206009097206260869_1577542998571&hid=e154acf7203ae1251e181abf8aca82a7&cmd=games&leagues=3&nhlLine=A&mblLine=A&lineStyle=E&language=0&_=1577542998572'
+	#SportBetReducedURLS='https://www.sportbet.com/lines/Basketball/Reduced/'
+
+
+	p=Process(target=intertopsBaseScraper,args=(IntertopsBaseURLs,masterdata))
+	p.start()
+	#p1=Process(target=sportBetBaseScraper,args=(SportBetReducedURLS,masterdata))
+	#p1.start()
+	p2=Process(target=bovadaBaseScraper,args=(bovadaAPIUrls,masterdata))
+	p2.start()
+	p3=Process(target=betPhoenixBaseScraper,args=(betPhoenixAPIUrls,masterdata))
+	p3.start()
+	p4=Process(target=skyBookBaseScraper,args=(skyBookAPIUrls,masterdata))
+	p4.start()
+
+
+
+
+
+
+
+	p.join()
+	#p1.join()
+	p2.join()
+	p3.join()
+	p4.join()
+	spreadData=[]
+	for key in masterdata:
+		entry={}
+		if len(masterdata[key])<2:
+		       continue
+		entry['AwayTeam']=masterdata[key][0]['AwayTeam'][2:-1].title()
+		entry['HomeTeam']=masterdata[key][0]['HomeTeam'][2:-1].title()
+		
+		for key1 in masterdata[key]:
+			if key1['LineType']=="Match":
+				bookName=key1['SportsBook']
+				entry[bookName+"AwayPointSpread"]=key1['AwayTeamPointSpreadAmerican']
+				entry[bookName+"AwayPointLine"]=round(float(key1['AwayTeamPointLineAmerican']),2)
+				entry[bookName+"HomePointSpread"]=key1['HomeTeamPointSpreadAmerican']
+				entry[bookName+"HomePointLine"]=round(float(key1['HomeTeamPointLineAmerican']),2)
+		spreadData.append(entry)
+			
+	print(spreadData)	
+	output=json.dumps(spreadData)
+	return output
+
 @app.route('/api/v1/resources/nba/match/spreads/decimal', methods=['GET'])
 def nba():
 	league='nba'
